ab0398cb26083d56684cdf90b46bc7512463aaf0
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-11-06 23:22:04 -0800

    Update loader

--- lua/gm_express_bindings/sh_init.lua
+++ lua/gm_express_bindings/sh_init.lua
@@ -4,6 +4,10 @@ AddCSLuaFile()
 if SERVER then
     include( "fpp/sv_init.lua" )
     AddCSLuaFile( "fpp/cl_init.lua" )
+
+    include( "adv2/sv_init.lua" )
+    AddCSLuaFile( "adv2/cl_init.lua" )
 else
     incluide( "fpp/cl_init.lua" )
+    incluide( "adv2/cl_init.lua" )
 end
