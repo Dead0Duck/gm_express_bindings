b5340b63fab9908e97c74e2870924eaafa64cd77
Brandon Sturgeon <brandon@brandonsturgeon.com>
2023-01-04 00:52:31 -0800

    wip

--- lua/gm_express_bindings/p2m/cl_init.lua
+++ lua/gm_express_bindings/p2m/cl_init.lua
@@ -1,89 +1,84 @@
 local enabled = CreateConVar( "express_enable_p2m", "1", FCVAR_ARCHIVE + FCVAR_REPLICATED, "Enable Prop2Mesh Bindings" )
+local originalUploadStartReceiver
 
-local function makeEnv( overrides )
-    return setmetatable( overrides or {}, {
-        __index = _G
-    } )
+local function mockNetStream()
+    return {
+        numchunks = 1,
+        clients = {
+            {
+                progress = 0,
+                finished = false,
+            }
+        }
+    }
 end
 
 local function wrapUploadStartReceiver()
-    local eid
-    local objectCount
+    originalUploadStartReceiver = originalUploadStartReceiver or net.Receivers["prop2mesh_upload_start"]
 
-    local nextObj
-    local objects = {}
+    local upstreams
+    local filecache_keys
 
-    local env = makeEnv( {
-        upstreams = prop2mesh.upstreams,
-        filecache_keys = prop2mesh.filecache_keys,
+    do
+        local name, func = debug.getupvalue( originalUploadStartReceiver, 2 )
+        assert( name == "filecache_keys", "Express: Failed to get upvalue 'filecache_keys' from 'prop2mesh_upload_start'" )
+        filecache_keys = func
+    end
 
-        -- This function only uses the WriteString function of prop2mesh
-        -- We just need to keep track of the data and give it a fake netstream object
-        prop2mesh = {
-            WriteStream = function( data )
-                nextObj.data = data
-                return {
-                    numChunks = 1,
-                    clients = {
-                        {
-                            progress = 0,
-                            finished = false,
-                        }
-                    }
-                }
-            end
-        },
-        net = {
-            ReadString = net.ReadString,
-            WriteUInt = net.WriteUInt,
+    do
+        local name, func = debug.getupvalue( originalUploadStartReceiver, 3 )
+        assert( name == "upstreams", "Express: Failed to get upvalue 'upstreams' from 'prop2mesh_upload_start'" )
+        upstreams = func
+    end
 
-            ReadUInt = function( bits )
-                local result = net.ReadUInt( bits )
+    local newReceiver = function()
+        local objects = {}
+        local eid = net.ReadUInt( 16 )
 
-                if bits == 16 and not eid then
-                    eid = result
-                elseif bits == 8 and not objectCount then
-                    objectCount = result
-                end
+        for _ = 1, net.ReadUInt( 8 ) do
+            local crc = net.ReadString()
+            local key = filecache_keys[crc]
+            local data = key and key.data
 
-                return result
-            end,
-
-            Start = function( message )
-                nextObj = {}
-                net.Start( message )
-            end,
-
-            WriteString = function( str )
-                nextObj.crc = str
-                net.WriteString( str )
-            end,
-
-            SendToServer = function()
-                table.insert( objects, nextObj )
+            if data then
+                net.Start( "prop2mesh_upload" )
+                net.WriteUInt( eid, 16 )
+                net.WriteString( crc )
+                upstreams[crc] = mockNetStream()
                 net.SendToServer()
 
-                -- The loop has completed, we can send the full dataset
-                if #objects == objectCount then
-                    print( "Uploading " .. objectCount .. " p2m objects to the server" )
-                    express.Send( "prop2mesh_upload", {
-                        eid = eid,
-                        objects = objects
-                    }, function()
-                        -- Update the fake netstream objects to be finished
-                        for _, obj in ipairs( objects ) do
-                            prop2mesh.upstreams[obj.crc].clients[1].finished = true
-                        end
-                    end )
+                table.insert( objects, { crc = crc, data = data } )
+            end
+        end
+
+        local objectCount = #objects
+        if objectCount == 0 then
+            ErrorNoHaltWithStack( "Express: No valid objects found in p2m upload request" )
+            return
+        end
+
+        print( "Uploading " .. objectCount .. " p2m objects to the server" )
+        express.Send(
+            "prop2mesh_upload",
+            { eid = eid, objects = objects },
+            function()
+                -- Update the fake netstream objects to be finished
+                for _, obj in ipairs( objects ) do
+                    prop2mesh.upstreams[obj.crc].clients[1].finished = true
                 end
             end
-        }
-    } )
+        )
+    end
 
-    setfenv( net.Receivers["prop2mesh_upload_start"], env )
+    net.Receive( "prop2mesh_upload_start", newReceiver )
 end
 
-local function setupReceivers()
+local function enable()
+    if not prop2mesh then return end
+    if not enabled:GetBool() then return end
+
+    wrapUploadStartReceiver()
+
     express.Receive( "prop2mesh_download", function( objects )
         prop2mesh.downloads = prop2mesh.downloads + #objects
 
@@ -95,23 +90,11 @@ local function setupReceivers()
     end )
 end
 
-local function unwrapUploadStartReceiver()
-    setfenv( net.Receivers["prop2mesh_upload_start"], _G )
-end
-
-local function enable()
-    if not prop2mesh then return end
-    if not enabled:GetBool() then return end
-
-    wrapUploadStartReceiver()
-    setupReceivers()
-end
-
 local function disable()
     if not prop2mesh then return end
     if enabled:GetBool() then return end
 
-    unwrapUploadStartReceiver()
+    net.Receive( "prop2mesh_upload_start", originalUploadStartReceiver )
 end
 
 cvars.AddChangeCallback( "express_enable_p2m", function( _, new )
