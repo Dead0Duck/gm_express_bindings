6e1559146e6daa031a0251df6c4c6feca62f7da4
Brandon Sturgeon <brandon@brandonsturgeon.com>
2023-01-17 14:25:15 -0800

    Make adv2 more resilient

--- lua/gm_express_bindings/adv2/sv_init.lua
+++ lua/gm_express_bindings/adv2/sv_init.lua
@@ -19,30 +19,36 @@ local function enable()
 
         print( SysTime(), "Running SendToClient, sending advdupe2_receivefile" )
         express.Send( "advdupe2_receivefile", sendData, ply, function( cbPly )
-            print( SysTime(), "advdupe2_receivefile callback for", cbPly )
             cbPly.AdvDupe2.Downloading = false
+            AdvDupe2.RemoveProgressBar( ply )
         end )
     end
 
     express.Receive( "advdupe2_receivefile", function( ply, data )
-        print( SysTime(), "Received advdupe2_receivefile from", ply )
         if not IsValid( ply ) then return end
         ply.AdvDupe2.Uploading = true
         AdvDupe2.InitProgressBar( ply, "Opening: " )
 
-        local name = data.name
-        local fileData = data.data
+        local success, err = pcall( function()
+            local name = data.name
+            local fileData = data.data
 
-        if not ply.AdvDupe2 then ply.AdvDupe2 = {} end
+            if not ply.AdvDupe2 then ply.AdvDupe2 = {} end
 
-        local _1, _2, _3 = string.find( name, "([%w_]+)" )
-        if _3 then
-            ply.AdvDupe2.Name = string.sub( _3, 1, 32 )
-        else
-            ply.AdvDupe2.Name = "Advanced Duplication"
+            local _1, _2, _3 = string.find( name, "([%w_]+)" )
+            if _3 then
+                ply.AdvDupe2.Name = string.sub( _3, 1, 32 )
+            else
+                ply.AdvDupe2.Name = "Advanced Duplication"
+            end
+
+            AdvDupe2.LoadDupe( ply, AdvDupe2.Decode( fileData ) )
+        end )
+
+        if not success then
+            ErrorNoHalt( "Error with received adv2 dupe, could not load dupe:", err )
         end
 
-        AdvDupe2.LoadDupe( ply, AdvDupe2.Decode( fileData ) )
         ply.AdvDupe2.Uploading = false
         AdvDupe2.RemoveProgressBar( ply )
     end )
